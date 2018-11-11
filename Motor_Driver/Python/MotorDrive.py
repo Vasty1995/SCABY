cfa12fd9a2f3c0ed09749726b0ce14670ab43e5c
Ayertena <43557195+Ayertena@users.noreply.github.com>
2018-11-10 23:16:28 -0800

    Update MotorDrive.py

--- Motor_Driver/Python/MotorDrive.py
+++ Motor_Driver/Python/MotorDrive.py
@@ -5,40 +5,40 @@ GPIO.setwarnings(False) #do not show any warnings
 GPIO.setmode(GPIO.BCM)
 
 # Right Motor      
-motorRENPin  = 2  # GPIO23
+motorRENPin  = 2  # GPIO2
 motorRIN2Pin = 3  # GPIO3
 motorRIN1Pin = 4  # GPIO4
 
 # Left Motor 
 motorLENPin  = 17 # GPIO17
-motorLIN2Pin = 27 # GPIO 27 
+motorLIN2Pin = 27 # GPIO27 
 motorLIN1Pin = 22 # GPIO22
 
 
 # Right Motor 
-GPIO.setup(motorRENPin, GPIO.OUT) # Ena GPIO2
-GPIO.setup(motorRIN2Pin, GPIO.OUT) # GPIO 3
-GPIO.setup(motorRIN1Pin, GPIO.OUT) # GPIO 4
-
-# Intialize turn motor off at first 
-GPIO.output(motorRENPin, GPIO.LOW) # GPIO2 = 0
+GPIO.setup(motorRENPin, GPIO.OUT)  # GPIO2 : Right Motor Enable pin 
+GPIO.setup(motorRIN2Pin, GPIO.OUT) # GPIO3
+GPIO.setup(motorRIN1Pin, GPIO.OUT) # GPIO4
 
 #Left Motor 
-GPIO.setup(motorLENPin,  GPIO.OUT) 
-GPIO.setup(motorLIN2Pin, GPIO.OUT)
-GPIO.setup(motorLIN1Pin, GPIO.OUT)
+GPIO.setup(motorLENPin,  GPIO.OUT) # GPIO17 : Left Motor Enable pin 
+GPIO.setup(motorLIN2Pin, GPIO.OUT) # GPIO27
+GPIO.setup(motorLIN1Pin, GPIO.OUT) # GPIO22
 
-# Intialize turn motor off at first 
-GPIO.output(motorLENPin, GPIO.LOW)
+# Start both motors from an off state.
+GPIO.output(motorRENPin, GPIO.LOW) # GPIO2 = 0
+GPIO.output(motorLENPin, GPIO.LOW) # GPIO17 = 0
 
 ###################################################
 # PWM
-p = GPIO.PWM(2,207) #10 blue
-p = GPIO.PWM(17,207) #5 brown
-p.start(0)
+# To start pwm => p = GPIO.PWM(Pin# , frequency)
+p = GPIO.PWM(2,207)  # Pin 2 @ f = 207Hz
+p = GPIO.PWM(17,207) # Pin 17 @f = 207Hz
+p.start(0) # p.start(dc), dc = duty cycle 
+           # (0.0 <= dc <= 100.0)
+# To chane frequency: p.ChangeFrequncy(freq) where freq is the new frequncy in Hz
+# To stop PWM: p.stop()
 
-
-#p.ChangeDutyCycle(90)
 try:
     while 1:
         for i in range(100):
@@ -46,7 +46,7 @@ try:
                 time.sleep(0.02)
                 
         # Right Motor 
-        GPIO.output(motorRIN1Pin, GPIO.HIGH) # GPIO 4, 8
+        GPIO.output(motorRIN1Pin, GPIO.HIGH) # 
         GPIO.output(motorRIN2Pin, GPIO.LOW) # GPIO 3, 9
         time.sleep(0.02)
     
