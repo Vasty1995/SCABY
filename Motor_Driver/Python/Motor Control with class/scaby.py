279c45f36013382f3bf4e865c939df2756e4f4f7
Ayertena <43557195+Ayertena@users.noreply.github.com>
2018-11-19 13:25:20 -0800

    Update scaby.py

--- Motor_Driver/Python/Motor Control with class/scaby.py
+++ Motor_Driver/Python/Motor Control with class/scaby.py
@@ -1,6 +1,9 @@
-# class class ClassName:
-    <statement-1>
-    .
-    .
-    .
-    <statement-N>
+class Motor:
+    def _init_(self, R_Ena, L_Ena, In1, In2, In3, In4) :
+        self.R_Ena = R_Ena
+        self.L_Ena = L_Ena
+        self.In1 = In1
+        self.In2 = In2
+        self.In3 = In3
+        self.In4 = In4
+        # whats next ?
