5f262d4f575b74b3b3c603d65dc67bcb68f1094e
Ayertena <43557195+Ayertena@users.noreply.github.com>
2018-11-20 12:12:24 -0800

    Update scaby.py

--- Motor_Driver/Python/Motor Control with class/scaby.py
+++ Motor_Driver/Python/Motor Control with class/scaby.py
@@ -1,23 +1,23 @@
-from Motor import Drive  
-import RPi.GPIO as GPIO
-import time 
+from Motor import Drive
+import RP.GPIO as GPIO
+import time
+#GPIO.setwarnings(False)
+#GPIO.setmode(GPIO.BCM)
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+#############################################3
+# Initialize Rspi pins to be used
 
-R_Motor_Ena = 0
-L_Motor_Ena = 26
-R_In1 = 5
-R_In2 = 6
-L_In3 = 13
-L_In4 = 19
+R_Motor_Ena = 0     # Right Motor Enable
+L_Motor_Ena = 26    # Left Motor Enable
+R_In1 = 5           # Right Motor pin 1
+R_In2 = 6           # Right Motor pin 2
+L_In1 = 13          # Left Motor pin 1
+L_In2 = 19          # Left Motor pin 2
+
 
 Right_Motor = Drive(R_In1, R_In2, R_Motor_Ena)
-Left_Motor = Drive(L_In3, L_In4, L_Motor_Ena)
+Left_Motor = Drive(L_In1, L_In2, L_Motor_Ena)
 
-Right_Motor.Forward
-Left_Motor.Forward
-time.sleep(5)
-
-
+Right_Motor.forward()
+Left_Motor.forward()
 
