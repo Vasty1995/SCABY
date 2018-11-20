ad511385fd9e0d407aa5265b07706864fb4c07ac
Ayertena <43557195+Ayertena@users.noreply.github.com>
2018-11-20 06:39:48 -0800

    Update Motor.py

--- Motor_Driver/Python/Motor Control with class/Motor.py
+++ Motor_Driver/Python/Motor Control with class/Motor.py
@@ -1,20 +1,14 @@
-class Motor:
-    def _init_(self, R_Ena, L_Ena, In1, In2, In3, In4) :
-        self.R_Ena = R_Ena
-        self.L_Ena = L_Ena
-        self.In1 = In1
-        self.In2 = In2
-        self.In3 = In3
-        self.In4 = In4
-        # whats next ?
-   
-    def setup_pins(setup):
-        GPIO.setup(setup.R_Ena, GPIO.OUT)
-        GPIO.setup(setup.L_Ena, GPIO.OUT)
-        GPIO.setup(setup.In1, GPIO.OUT)
-        GPIO.setup(setup.In2, GPIO.OUT)
-        GPIO.setup(setup.In3, GPIO.OUT)
-        GPIO.setup(setup.In4, GPIO.OUT)
-    
-   
+class Drive:
+    def _init_(self, pin1, pin2, pinEna):
+        self.pin1 = pin1
+        self.pin2 = pin2
+        self.pinEna = pinEna
+            
+    def forward(self):
+        GPIO.output(self.pin1, 1)
+        GPIO.output(self.pin2, 0)
+        GPIO.output(self.pinEna, 1)
         
+  #  def Reverse(self): 
+        
+       
