6a00b4aa0c6b79537fa045a88f4f9ef1b074685e
Ayertena <43557195+Ayertena@users.noreply.github.com>
2018-11-20 17:54:37 -0800

    Update Motor.py

--- Motor_Driver/Python/Motor Control with class/Motor.py
+++ Motor_Driver/Python/Motor Control with class/Motor.py
@@ -1,43 +1,44 @@
+# Motor.py
+#
+import RPi.GPIO as GPIO
 class Drive:
     pin1 = 0
     pin2 = 0
     pinEN = 0
-
+    
+    GPIO.output(self.pinEN, 0)
+    
     def __init__(self, pin1, pin2, pinEN):
         self.pin1 = pin1
         self.pin2 = pin2
         self.pinEN = pinEN
-        """
-    
-        GPIO.setup(self.pin1, GPIO.OUTPUT)
-        GPIO.setup(self.pin2, GPIO.OUTPUT)
-        GPIO.setup(self.pinEN, GPIO.OUTPUT)
         
-        GPIO.output(self.pinEN, 0)
-        """
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+
+        GPIO.setup(self.pin1, GPIO.OUT)
+        GPIO.setup(self.pin2, GPIO.OUT)
+        GPIO.setup(self.pinEN, GPIO.OUT)
+        
     def forward(self):
-        return self.pin1
-        print self.pin2
-        print self.pinEN
+        GPIO.output(self.pin1, 0)
+        GPIO.output(self.pin2, 1)
+        GPIO.output(self.pinEN,1)
 
-
-"""    return GPIO.output(self.pin1, 1)
-        return GPIO.output(self.pin2, 0)
-        return GPIO.output(self.pinEna, 1)
-
-
+    def break_M(self):
+        GPIO.output(self.pin1, 0)
+        GPIO.output(self.pin2, 0)
+        GPIO.output(self.pinEN, 1)
+"""
 ##############################################
-
 DC Motors can turn in two directions.
 The turn directions are controlled by the power being applied to the motor.
-
 An H Bridge is used to control => direction of the motor. 
                                => speed 
                                => break 
   1. L298 H Bridge  
      H Bridge: a circuit which allows a voltage to be applied across a load (motor) in either direction 
              : has 4 switches arranged in an "H" fashion 
-
     L298 H Bridge Hardware Logic:
        Left                                         Right
      _____________________________________________
@@ -47,15 +48,11 @@ An H Bridge is used to control => direction of the motor.
     _              |         |________|           | 
     |  L_pin2 = 2  |                              | 4 = R_pin4
     |______________|______________________________|
-
         L_pin1 = IN1 && EN 
         L_pin2 = ~IN1 && EN
-
         R_pin3 = IN2 && EN
         R_pin3 = ~IN2 && EN
-
         Out1 & Out2 can only be either postive(closed) or negative(open)    
-
  ###########
  Forward Drive (spin Motor Clockwise) 
  
@@ -90,7 +87,6 @@ An H Bridge is used to control => direction of the motor.
         L_pin1 = IN1 && EN =  1 & 1 = 1  |   R_pin3 = IN2 && EN  = 0 & 1 = 0 
         L_pin2 = ~IN1 && EN = 0 & 1 = 0  |   R_pin4 = ~IN2 && EN = 1 & 1 = 1  
         
-
  ###########
  First Break Motor 
  
@@ -132,4 +128,6 @@ An H Bridge is used to control => direction of the motor.
         
        
 """
+
+
 
