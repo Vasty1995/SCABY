868b5125ff08858cc4282e75c74a42b46ee31275
Ayertena <43557195+Ayertena@users.noreply.github.com>
2019-04-14 14:01:14 -0700

    Update marvelmind.c

--- scaby/marvelmind.c
+++ scaby/marvelmind.c
@@ -937,38 +937,49 @@ bool getStationaryBeaconsPositionsFromMarvelmindHedge (struct MarvelmindHedge * 
     return true;
 }
 
-void printStationaryBeaconsPositionsFromMarvelmindHedge (struct MarvelmindHedge * hedge,
-                                                         bool onlyNew)
-{struct StationaryBeaconsPositions positions;
-  double xm,ym,zm;
-
-    getStationaryBeaconsPositionsFromMarvelmindHedge(hedge, &positions);
+void printStationaryBeaconsPositionsFromMarvelmindHedge (struct MarvelmindHedge * hedge, bool onlyNew)
+{
+	
+    // char station[120];
+    // int len = strlen(station);
+    
+    struct StationaryBeaconsPositions positions;
+  	double xm,ym,zm;
+   
+   getStationaryBeaconsPositionsFromMarvelmindHedge(hedge, &positions);
 
     if (positions.updated || (!onlyNew))
-    {uint8_t i;
-     uint8_t n= hedge->positionsBeacons.numBeacons;
-     struct StationaryBeaconPosition *b;
+    {
+		uint8_t i;
+        uint8_t j;
+        uint8_t n= hedge->positionsBeacons.numBeacons;
+        struct StationaryBeaconPosition *b;
 
-        for(i=0;i<n;i++)
-        {
-            b= &positions.beacons[i];
-            xm= ((double) b->x)/1000.0;
-            ym= ((double) b->y)/1000.0;
-            zm= ((double) b->z)/1000.0;
-            if (positions.beacons[i].highResolution)
-            {
-                printf ("Stationary beacon: address: %d, X: %.3f, Y: %.3f, Z: %.3f \n",
-                            b->address,xm, ym, zm);
-            } else
-            {
-                printf ("Stationary beacon: address: %d, X: %.2f, Y: %.2f, Z: %.2f \n",
-                            b->address,xm, ym, zm);
+        for(i=0;i<n;i++){
+                b= &positions.beacons[i];
+                xm= ((double) b->x)/1000.0;
+                ym= ((double) b->y)/1000.0;
+                zm= ((double) b->z)/1000.0;
+            
+                if (positions.beacons[i].highResolution){
+                    FILE *fp;
+                    fp = fopen("t.txt","a");
+                        
+                        printf("%d, %.3f, %.3f, %.3f\n", b->address, xm, ym, zm); 
+                        
+                        fprintf(fp, "%d, %.3f, %.3f, %.3f\n", b->address, xm, ym, zm);
+                        
+                        fclose(fp);
+                        }  
+                        
+                else {
+                        printf("Jumped into else");
+                    }
             }
-        }
-
+        
         hedge->positionsBeacons.updated= false;
     }
-}
+    }
 
 //////////////
 
